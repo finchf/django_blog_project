9f937fc7854760701c0f4b8f8e07b5eb224418d5
Dmitrij <dmbor123@gmail.com>
2023-10-19 15:24:36 +0300

    edit urls and views

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,24 @@
 from django.urls import path
-from blog.views import *
+from .views import about_blog, show_home,set_password, set_userdata, register, deacivate_account, \
+    login, logout, view_topics, add_comment, create_article, article_update, article_delete, view_article,\
+    subscribe_topic, unsubscribe_topic, user_profile
 
 urlpatterns = [
-    path("about/", about_blog),
+    path("about/", about_blog, name='about'),
     path("", show_home, name='home'),
-    path("set-password/",set_password),
-    path("set-userdata/", set_userdata),
-    path("register/", create_new_user),
-    path("deactivate/", deacivate_account),
-    path("login/", login_page),
-    path("logout/", logout_user),
-    path("topics/", view_topics),
-    path("<str:name>/comment/", article_comment, name='article'),
-    path("create/", create_article),
-    path("<article>/update/", article_update),
-    path("<article>/delete/", article_delete),
-    path("<str:name>/", view_article, name='article'),
-    path("topics/<str:topic>/", topic_change),
-    path("topics/<str:topic>/subscribe/", subscribe_topic),
-    path("topics/<str:topic>/unsubscribe/", unsubscribe_topic),
-    path("profile/<str:username>/", user_profile),
+    path("set-password/",set_password, name='set_password'),
+    path("set-userdata/", set_userdata, name='set_userdata'),
+    path("register/", register, name='register'),
+    path("deactivate/", deacivate_account, name='deactivate'),
+    path("login/", login, name='login'),
+    path("logout/", logout, name='logout'),
+    path("create/", create_article, name='create'),
+    path("<int:article_id>/", view_article, name='detail'),
+    path("<int:article_id>/comment/", add_comment, name='add_comment'),
+    path("<int:article_id>/update/", article_update, name='article_update'),
+    path("<int:article_id>/delete/", article_delete, name='article_delete'),
+    path("topics/", view_topics, name='topics'),
+    path("topics/<int:topic_id>/subscribe/", subscribe_topic, name='subscribe_topic'),
+    path("topics/<int:topic_id>/unsubscribe/", unsubscribe_topic, name='unsubscribe_topic'),
+    path("profile/<username>/", user_profile, name='user_profile'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,39 +10,36 @@ def show_home(request: HttpRequest) -> HttpResponse:
     return HttpResponse(f"This is home page")
 
 
-def view_article(request: HttpRequest, name: str) -> HttpResponse:
-    return HttpResponse(f"This is page of article {name}")
+def view_article(request: HttpRequest, article_id: int) -> HttpResponse:
+    return HttpResponse(f"This is page of article {article_id}")
 
 
-def article_comment(request: HttpRequest, name: str,) -> HttpResponse:
-    return HttpResponse(f"This is comment of article {name}")
+def add_comment(request: HttpRequest, article_id: int) -> HttpResponse:
+    return HttpResponse(f"This is comment of article {article_id}")
 
 
 def create_article(request: HttpRequest) -> HttpResponse:
     return HttpResponse(f"This is the article creation form page")
 
 
-def article_update(request: HttpRequest, article: str) -> HttpResponse:
-    return HttpResponse(f"This is page for update  {article} ")
+def article_update(request: HttpRequest, article_id: int) -> HttpResponse:
+    return HttpResponse(f"This is page for update  {article_id} ")
 
 
-def article_delete(request: HttpRequest, article: str) -> HttpResponse:
-    return HttpResponse(f"This is page for delete  {article} ")
+def article_delete(request: HttpRequest, article_id: int) -> HttpResponse:
+    return HttpResponse(f"This is page for delete  {article_id} ")
 
 
 def view_topics(request: HttpRequest) -> HttpResponse:
     return HttpResponse(f"This is all topics on blog")
 
 
-def topic_change(request: HttpRequest, topic: str) -> HttpResponse:
-    return HttpResponse(f"Topic_change")
 
-
-def subscribe_topic(request: HttpRequest, topic: str) -> HttpResponse:
+def subscribe_topic(request: HttpRequest, topic_id: int) -> HttpResponse:
     return HttpResponse(f"Subscribe topic")
 
 
-def unsubscribe_topic(request: HttpRequest, topic: str) -> HttpResponse:
+def unsubscribe_topic(request: HttpRequest, topic_id: int) -> HttpResponse:
     return HttpResponse(f"Unsubscribe topic")
 
 
@@ -62,14 +59,14 @@ def deacivate_account(request: HttpRequest) -> HttpResponse:
     return HttpResponse(f"On this page you can deactivate your account")
 
 
-def create_new_user(request: HttpRequest) -> HttpResponse:
+def register(request: HttpRequest) -> HttpResponse:
     return HttpResponse(f"Create new profile!")
 
 
-def login_page(request: HttpRequest) -> HttpResponse:
+def login(request: HttpRequest) -> HttpResponse:
     return HttpResponse(f"Hello! log in, please!")
 
 
-def logout_user(request: HttpRequest) -> redirect:
+def logout(request: HttpRequest) -> redirect:
     logout(request)
     return redirect("home")
