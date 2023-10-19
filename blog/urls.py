869232bd0f79c0e86605a984cfe8a434b7025b06
Dmitrij <dmbor123@gmail.com>
2023-10-19 07:01:30 +0300

    add url and views

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,23 @@
 from django.urls import path
+from blog.views import *
 
 urlpatterns = [
-
+    path("about/", about_blog),
+    path("", show_home, name='home'),
+    path("set-password/",set_password),
+    path("set-userdata/", set_userdata),
+    path("register/", create_new_user),
+    path("deactivate/", deacivate_account),
+    path("login/", login_page),
+    path("logout/", logout_user),
+    path("topics/", view_topics),
+    path("<str:name>/comment/", article_comment, name='article'),
+    path("create/", create_article),
+    path("<article>/update/", article_update),
+    path("<article>/delete/", article_delete),
+    path("<str:name>/", view_article, name='article'),
+    path("topics/<str:topic>/", topic_change),
+    path("topics/<str:topic>/subscribe/", subscribe_topic),
+    path("topics/<str:topic>/unsubscribe/", unsubscribe_topic),
+    path("profile/<str:username>/", user_profile),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,75 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import redirect
 
-def about(request):
-    return HttpResponse('about blog')
+def about_blog(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"This is blog")
 
+
+def show_home(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"This is home page")
+
+
+def view_article(request: HttpRequest, name: str) -> HttpResponse:
+    return HttpResponse(f"This is page of article {name}")
+
+
+def article_comment(request: HttpRequest, name: str,) -> HttpResponse:
+    return HttpResponse(f"This is comment of article {name}")
+
+
+def create_article(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"This is the article creation form page")
+
+
+def article_update(request: HttpRequest, article: str) -> HttpResponse:
+    return HttpResponse(f"This is page for update  {article} ")
+
+
+def article_delete(request: HttpRequest, article: str) -> HttpResponse:
+    return HttpResponse(f"This is page for delete  {article} ")
+
+
+def view_topics(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"This is all topics on blog")
+
+
+def topic_change(request: HttpRequest, topic: str) -> HttpResponse:
+    return HttpResponse(f"Topic_change")
+
+
+def subscribe_topic(request: HttpRequest, topic: str) -> HttpResponse:
+    return HttpResponse(f"Subscribe topic")
+
+
+def unsubscribe_topic(request: HttpRequest, topic: str) -> HttpResponse:
+    return HttpResponse(f"Unsubscribe topic")
+
+
+def user_profile(request: HttpRequest, username: str) -> HttpResponse:
+    return HttpResponse(f"Username is {username}")
+
+
+def set_password(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"Please, set your new password")
+
+
+def set_userdata(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"Please, update user info")
+
+
+def deacivate_account(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"On this page you can deactivate your account")
+
+
+def create_new_user(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"Create new profile!")
+
+
+def login_page(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"Hello! log in, please!")
+
+
+def logout_user(request: HttpRequest) -> redirect:
+    logout(request)
+    return redirect("home")
