48d2ed1bc54038ed089dcebdc2ec04cdb6035b17
Dmitrij <dmbor123@gmail.com>
2023-10-20 00:49:12 +0300

    created models and admin files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=255)
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topics = models.ManyToManyField(Topic)
+
+class Comment(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class UserTopic(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    prefers = models.BooleanField(default=False)
+    notify = models.BooleanField(default=False)
+
+
+
+
--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'django_blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
